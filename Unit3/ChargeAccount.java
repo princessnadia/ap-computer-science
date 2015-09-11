c658438c251953bd7a4fe5369ad48a4e7d3c1314
Marlene Yuuki <princess.nadia.1019@gmail.com>
2015-09-11 10:48:01 -0500

    unit 3 day 5

--- Unit3/ChargeAccount.java
+++ Unit3/ChargeAccount.java
@@ -7,14 +7,17 @@ public class ChargeAccount
    {
        Scanner inp = new Scanner(System.in);
        NumberFormat dol = NumberFormat.getCurrencyInstance();
-       double bal, add, ntr, nwb, min;
+       double bal, add, ntr, nwb = 0.0, min = 0.0;
        // last time
        System.out.print("Previous balance: ");
        bal = inp.nextDouble();
-       System.out.println("Additional charges: ");
+       System.out.print("Additional charges: ");
        add = inp.nextDouble();
-       
-       
+       if (bal > 0.0) ntr = (bal + add) * 0.02;
+       else ntr = 0.0;
+       nwb = bal + add + ntr;
+       if (nwb < 50.0) min = nwb;
+       else if (nwb <= 300.0 || nwb >= 50.0) min = 50.0;
        // statement
        System.out.print("\f");
        System.out.println("CS CARD International Statement\n===============================\n\n");
---
+++ Unit3/Dates.java
@@ -0,0 +1,75 @@
+// ****************************************************************
+
+// Dates.java
+
+//
+
+// Determine whether a 2nd-millenium date entered by the user
+
+// is valid         
+
+// ****************************************************************
+
+import java.util.Scanner;
+
+ 
+
+public class Dates
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+      int mm = 0, dd = 0, yy = 0, ddimm = 0;
+
+      boolean mmv, yyv, ddv, lyy;
+
+      Scanner inp = new Scanner(System.in);
+
+      System.out.print("Enter month (1-12): ");
+      mm = inp.nextInt();
+      
+      System.out.print("Enter day (1-31): ");
+      dd = inp.nextInt();
+      
+      System.out.print("Enter year (1000-1999): ");
+      yy = inp.nextInt();
+      
+      if (mm >= 1 && mm <= 12) mmv = true;
+      else mmv = false;
+
+      if (yy >= 1000 && yy <= 1999) yyv = true;
+      else mmv = false;
+
+      if (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0)) lyy = true;
+      else lyy = false;
+
+ 
+
+      //Determine number of days in month
+          switch (mm) {
+              case 1: ddimm = 31; break;
+              case 2: ddimm = 28; break;
+              case 3: ddimm = 31; break;
+              case 4: ddimm = 30; break;
+              case 5: ddimm = 31; break;
+              case 6: ddimm = 30; break;
+              case 7: ddimm = 31; break;
+              case 8: ddimm = 31; break;
+              case 9: ddimm = 30; break;
+              case 10: ddimm = 31; break;
+              case 11: ddimm = 30; break;
+              case 12: ddimm = 31; break;
+            }
+
+      if (dd <= ddimm) ddv = true;
+
+      if (ddv && mmv && yyv) System.out.print("Date is valid!");
+      else System.out.print("Date is invalid!");
+ 
+
+    }
+
+}
