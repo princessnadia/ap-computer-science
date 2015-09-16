c34f100e3991c67e71e76fe2d1e9a150dec93328
Marlene Yuuki <princess.nadia.1019@gmail.com>
2015-09-16 10:47:56 -0500

    unit 3 day 8

--- Unit3/Rock.java
+++ Unit3/Rock.java
@@ -26,10 +26,14 @@ public class Rock
       String computerPlay;  //Computer's play -- "R", "P", or "S"
       int computerInt;      //Randomly generated number used to determine
                             //computer's play
-      Scanner scan = new Scanner(System.in);
+      Scanner inp = new Scanner(System.in);
       Random gen = new Random();
+      System.out.println("Let's play!");
       //Get player's play -- note that this is stored as a string
+      System.out.print("Your move? (R, P, S): ");
       //Make player's play uppercase for ease of comparison
+      personPlay = inp.nextChar();
+      personPlay = personPlay.toUpperCase();
       //Generate computer's play (0,1,2)
       //Translate computer's randomly generated play to string
       switch (computerInt)
@@ -44,22 +48,8 @@ public class Rock
 
       //See who won.  Use nested ifs instead of &&.
 
-      if (personPlay.equals(computerPlay)) 
-
-          System.out.println("It's a tie!");
-
-      else if (personPlay.equals("R"))
-
-          if (computerPlay.equals("S"))
-
-            System.out.println("Rock crushes scissors.  You win!!");
-
-          else
-
- 
-
-            //...  Fill in rest of code
-
-    }
-
+      if (personPlay.equals(computerPlay)) System.out.println("It's a tie!");
+      else if (personPlay.equals("R")) {
+        }
+}
 }
