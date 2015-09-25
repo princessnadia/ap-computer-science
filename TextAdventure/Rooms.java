b01527a5788707e40978327096920cade4f89f04
Marlene Yuuki <princess.nadia.1019@gmail.com>
2015-09-25 10:01:20 -0500

    Revert "yay constructors"
    This reverts commit e1e4e1bb19e15f0c9f5bd5467400a8169c27ce9e.

--- TextAdventure/Rooms.java
+++ TextAdventure/Rooms.java
@@ -3,13 +3,6 @@ public class Rooms
     private String desc;
     public Rooms(int rmX, int rmY)
     {
-        if (rmX == 0 && rmY == 0) {
-            desc = "you are in a plain room, flooded by light above. there is a (door) behind you, and a (note) to your left.";
-            
-        }
-    }
-    public String desc()
-    {
-        return desc;
+        desc = "you are in a plain room, flooded by light above. there is a (door) behind you, and a (note) to your left.";
     }
 }
