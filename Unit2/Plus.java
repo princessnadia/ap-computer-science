b2f0e2814894dcb714c88b51052fd767918ce03e
Marlene Yuuki <princess.nadia.1019@gmail.com>
2015-11-03 10:46:23 -0600

    move updated stuff (d70.11.03.2)

--- Unit2/Plus.java
+++
@@ -1,8 +0,0 @@
-public class Plus
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Ten robins plus " + 13 + " canaries is " + (10 + 13) + " birds.");
-        System.out.println();
-    }
-}
