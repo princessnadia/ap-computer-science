83b36574b83b8187a6f8961a6fdc74c1b62fb006
Marlene Yuuki <princess.nadia.1019@gmail.com>
2015-11-12 10:48:08 -0600

    more work on help! (d70.11.12.2)

--- java/TallGrass/help.java
+++ java/TallGrass/help.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class help
 {
-	private Scanner inp = new Scanner(System.in);
-	public static void logo()
+    private static Scanner inp = new Scanner(System.in);
+    public static void logo()
     {
         System.out.println("  _____     _ _  ____                   ");
         System.out.println(" |_   _|_ _| | |/ ___|_ __ __ _ ___ ___ ");
@@ -10,14 +10,29 @@ public class help
         System.out.println("   | | (_| | | | |_| | | | (_| \\__ \\__ \\");
         System.out.println("   |_|\\__,_|_|_|\\____|_|  \\__,_|___/___/");
     }
-    
-	public static void menu()
-	{
-		logo();
-		System.out.println("-- TALLGRASS MANUAL --" +
-		"\n(Enter a number to go to the chapter.)"		+ 
-		"\n1) Introduction" +
-		"\n2) Battle" +
-		"\n3) Status Screen");
-	}
+
+    public static void menu()
+    {
+        boolean done = true;
+        do {
+            int inpt;
+            
+            logo();
+            System.out.println("-- TALLGRASS MANUAL --" +
+                "\n(Enter a number to go to the chapter.)"		+ 
+                "\n1) Introduction" +
+                "\n2) Battle" +
+                "\n3) Status Screen");
+            System.out.print(" - ");
+            try
+            {
+                inpt = inp.nextInt();
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println("Please enter a number.");
+                done = false;
+            }
+        } while (done);
+    }
 }
